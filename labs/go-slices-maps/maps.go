7735611b79069c04bce55280777d2af3b5e09aac
Alberto Labrada <albertolabrada99@gmail.com>
2020-03-09 11:53:39 -0600

    Solved go-slices-maps
    Signed-off-by: Alberto Labrada <albertolabrada99@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	ans := make(map[string]int)
+	for _, word := range strings.Fields(s) {
+		ans[word]++
+	}
+	return ans
+}
+
 func main() {
+	wc.Test(WordCount)
+
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,22 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var ans[][]uint8
+	for i := 0; i < dy; i++ {
+		ans = append(ans, make([]uint8, dx))
+	}
+
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			ans[y][x] = (uint8(x) + uint8(y)) / 2
+		}
+	}
+
+	return ans
+}
+
 func main() {
+	pic.Show(Pic)
 }
