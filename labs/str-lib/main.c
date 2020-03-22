2161791bb90f6df5b9cb66dd14bbf257f171bbd0
Alberto Labrada <albertolabrada99@gmail.com>
2020-03-22 11:07:36 -0700

    Solved str-lib
    Signed-off-by: Alberto Labrada <albertolabrada99@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *);
+char * mystradd(char *, char *);
+int mystrfind(char *str, char *substr);
+
+int main(int argc, char **argv) {
+
+    if (argc < 4) {
+        printf("Usage: ./main originalVeryLongString Addition Add\n");
+        return 1;
+    }
+
+    printf("%-25s : %-25d\n", "Initial Length ", mystrlen(argv[1]));
+    char * newString = mystradd(argv[1], argv[2]);
+    printf("%-25s : %-25s\n", "New string ", newString);
+    printf("%-25s : %-25s\n", "Substring was found ", mystrfind(newString, argv[3]) ? "yes" : "no");
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,39 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+
+    int c;
+    for (c = 0; str[c] != '\0'; c++)
+        ;
+    return c;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+
+    int i = 0;
+    char * newstr = malloc((mystrlen(origin) + mystrlen(addition) + 1) * sizeof(*newstr));
+
+    while (*origin != '\0')
+        newstr[i++] = *origin++;
+    while(*addition != '\0')
+        newstr[i++] = *addition++;
+    newstr[i] = '\0';
+
+    return newstr;
 }
 
+// 1: found, 0: not found
 int mystrfind(char *origin, char *substr){
+
+    while (*origin != '\0')
+    {
+        if (*origin == *substr && *substr != '\0')
+            substr++;
+        origin++;
+    }
+    
+    if (*substr == '\0') return 1;
+
     return 0;
 }
