5ee38e7165f3b6db82c02f48602a7dac02697907
Alberto Labrada <A01114148@itesm.mx>
2020-04-28 15:18:04 -0600

    Solved clockwall
    Signed-off-by: Alberto Labrada <A01114148@itesm.mx>

--- labs/clockwall/clock2.go
+++ labs/clockwall/clock2.go
@@ -2,16 +2,23 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
-func handleConn(c net.Conn) {
+func getName() string {
+
+	return "City"
+}
+
+func handleConn(c net.Conn, loc *time.Location) {
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, fmt.Sprintf("%-16s : %s", loc.String(), time.Now().In(loc).Format("15:04:05\n")))
 		if err != nil {
 			return // e.g., client disconnected
 		}
@@ -20,7 +27,21 @@ func handleConn(c net.Conn) {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+
+	if len(os.Args) < 2 {
+		fmt.Println("usage:(TZ=US/Eastern) go run clock2 -port <port> &")
+		os.Exit(1)
+	}
+
+	port := os.Args[2]
+	tz := os.Getenv("TZ")
+
+	loc, err := time.LoadLocation(tz)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listener, err := net.Listen("tcp", "localhost:"+port)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,6 +51,6 @@ func main() {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+		go handleConn(conn, loc) // handle connections concurrently
 	}
 }
