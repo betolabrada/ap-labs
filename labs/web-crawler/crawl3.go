aac1ad73676eaa7a68b176c629f8583aa348d19d
Alberto Labrada <A01114148@itesm.mx>
2020-05-17 13:16:50 -0600

    Solved webCrawler
    Signed-off-by: Alberto Labrada <A01114148@itesm.mx>

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -16,16 +16,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"gopl.io/ch5/links"
 )
+
+// Type nodes to keep track of depth of crawled links
+type nodes struct {
+	paths []string
+	depth int
+}
 
 //!+sema
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
-func crawl(url string) []string {
+func crawl(url string, parentDepth int) nodes {
 	fmt.Println(url)
 	tokens <- struct{}{} // acquire a token
 	list, err := links.Extract(url)
@@ -34,33 +42,49 @@ func crawl(url string) []string {
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+	return nodes{paths: list, depth: parentDepth + 1} // this is so we can return depth too
 }
 
 //!-sema
 
 //!+
 func main() {
-	worklist := make(chan []string)
-	var n int // number of pending sends to worklist
+	worklist := make(chan nodes) // Channel now is of type nodes
+	var n int                    // number of pending sends to worklist
 
+	if len(os.Args) < 3 {
+		log.Println("usage: ./crawl3 -depth=<int> <link ...>")
+		os.Exit(1)
+	}
+
+	depth, err := strconv.Atoi(os.Args[1][strings.Index(os.Args[1], "=")+1:])
+	if err != nil {
+		log.Println("error in depth number")
+		os.Exit(1)
+	}
 	// Start with the command-line arguments.
+	firstNodes := nodes{paths: os.Args[2:], depth: 0}
 	n++
-	go func() { worklist <- os.Args[1:] }()
+	go func() { worklist <- firstNodes }()
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
-		list := <-worklist
+		nodes := <-worklist      // receive the nodes
+		if nodes.depth > depth { // Check depth
+			continue
+		}
+		list := nodes.paths
 		for _, link := range list {
 			if !seen[link] {
 				seen[link] = true
 				n++
 				go func(link string) {
-					worklist <- crawl(link)
+					worklist <- crawl(link, nodes.depth) // pass in the depth
 				}(link)
 			}
 		}
+
 	}
 }
 
